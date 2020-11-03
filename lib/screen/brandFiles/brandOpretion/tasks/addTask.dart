fd5ba051889b83da16cffa5e04ecd3794f5ed6cc
rabab ebrahim <rababebrahim62@gmail.com>
2020-11-03 11:44:32 +0200

    view Static Employee Ids

--- lib/screen/brandFiles/brandOpretion/tasks/addTask.dart
+++ lib/screen/brandFiles/brandOpretion/tasks/addTask.dart
@@ -33,7 +33,7 @@ class _AddTaskPageState extends State<AddTaskPage> {
   final _formKey = GlobalKey<FormState>();
   final _key = GlobalKey<ScaffoldState>();
   bool processing;
-  List<String> employeesEmails = [];
+  List<String> employeesEmails =<String> ['', 'employee1', 'employee2', 'employee3', 'employee4'];
   String employeeEmail = '';
   bool _initialized = false;
   bool _error = false;
@@ -64,17 +64,17 @@ class _AddTaskPageState extends State<AddTaskPage> {
         text: isEditMode ? widget.taskModel.description : '');
     _taskDate = DateTime.now();
     processing = false;
-    getEmployees();
+    // getEmployees(); 
   }
 
-  getEmployees() {
-    employeesRef.get().then((QuerySnapshot querySnapshot) => {
-          querySnapshot.docs.forEach((doc) {
-            print(doc["email"]);
-            employeesEmails.add(doc["email"]);
-          })
-        });
-  }
+  // getEmployees() {
+  //   employeesRef.get().then((QuerySnapshot querySnapshot) => {
+  //         querySnapshot.docs.forEach((doc) {
+  //           employeesEmails.add(doc["email"]);
+  //           print(employeesEmails);
+  //         })
+  //       });
+  // }
 
   get isEditMode => widget.taskModel != null;
 
@@ -85,168 +85,160 @@ class _AddTaskPageState extends State<AddTaskPage> {
         title: Text(isEditMode ? 'Edit Task' : 'Add New Task'),
       ),
       key: _key,
-      // body: CustomScrollView(
-      //   slivers: <Widget>[
-      //     SliverList(
-      //       delegate: SliverChildListDelegate(
-      //         [
-      //           Container(
-      body: Form(
-        key: _formKey,
-        child: Container(
-          alignment: Alignment.center,
-          child: ListView(
-            children: <Widget>[
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _nameController,
-                  validator: (value) =>
-                      (value.isEmpty) ? "Please Enter Brand Name" : null,
-                  decoration: InputDecoration(
-                    labelText: "Name",
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _descriptionController,
-                  minLines: 3,
-                  maxLines: 5,
-                  validator: (value) =>
-                      (value.isEmpty) ? "Please Enter description" : null,
-                  decoration: InputDecoration(
-                    labelText: "description",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              ListTile(
-                title: Text("Date (YYYY-MM-DD)"),
-                subtitle: Text(
-                    "${_taskDate.year} - ${_taskDate.month} - ${_taskDate.day}"),
-                onTap: () async {
-                  DateTime picked = await showDatePicker(
-                      context: context,
-                      initialDate: _taskDate,
-                      firstDate: DateTime(_taskDate.year - 5),
-                      lastDate: DateTime(_taskDate.year + 5));
-                  if (picked != null) {
-                    setState(() {
-                      _taskDate = picked;
-                    });
-                  }
-                },
-              ),
-              SizedBox(height: 10.0),
-              new FormField(
-                builder: (FormFieldState state) {
-                  return InputDecorator(
+      
+      body: new SafeArea(
+        top: false,
+        bottom: false,
+        child: new Form(
+          key: _formKey,
+          child: Container(
+            alignment: Alignment.center,
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: TextFormField(
+                    controller: _nameController,
+                    validator: (value) =>
+                        (value.isEmpty) ? "Please Enter Brand Name" : null,
                     decoration: InputDecoration(
-                      icon: const Icon(Icons.person),
-                      labelText: 'Employees',
-                    ),
-                    isEmpty: employeeEmail == '',
-                    child: new DropdownButtonHideUnderline(
-                      child: new DropdownButton(
-                        value: employeeEmail,
-                        isDense: true,
-                        onChanged: (String newValue) {
-                          setState(() {
-                            
-                            employeeEmail = newValue;
-                            state.didChange(newValue);
-                          });
-                        },
-                        items: employeesEmails.map((String value) {
-                          return new DropdownMenuItem(
-                            value: value,
-                            child: new Text(value),
-                          );
-                        }).toList(),
+                      labelText: "Name",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                  );
-                },
-              ),
-              // FlatButton(
-              //   onPressed: () {
-              //     Navigator.push(
-              //       context,
-              //       MaterialPageRoute(
-              //         builder: (_) => ViewEmployeeList(),
-              //       ),
-              //     );
-              //   },
-              //   child: Text('Choose From Employee List'),
-              // ),
-              SizedBox(height: 20.0),
-              processing
-                  ? Center(child: CircularProgressIndicator())
-                  : Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Theme.of(context).primaryColor,
-                        child: MaterialButton(
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              setState(() {
-                                processing = true;
-                              });
-                              if (widget.taskModel != null) {
-                                TaskModel taskModel = TaskModel(
-                                  name: _nameController.text.trim(),
-                                  description:
-                                      _descriptionController.text.trim(),
-                                  id: widget.taskModel.id,
-                                );
-                                await NewTaskDB().updateTask(taskModel);
-                              } else {
-                                await NewTaskDB().addNewTask(
-                                  TaskModel(
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: TextFormField(
+                    controller: _descriptionController,
+                    minLines: 3,
+                    maxLines: 5,
+                    validator: (value) =>
+                        (value.isEmpty) ? "Please Enter description" : null,
+                    decoration: InputDecoration(
+                      labelText: "description",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                ListTile(
+                  title: Text("Date (YYYY-MM-DD)"),
+                  subtitle: Text(
+                      "${_taskDate.year} - ${_taskDate.month} - ${_taskDate.day}"),
+                  onTap: () async {
+                    DateTime picked = await showDatePicker(
+                        context: context,
+                        initialDate: _taskDate,
+                        firstDate: DateTime(_taskDate.year - 5),
+                        lastDate: DateTime(_taskDate.year + 5));
+                    if (picked != null) {
+                      setState(() {
+                        _taskDate = picked;
+                      });
+                    }
+                  },
+                ),
+                SizedBox(height: 10.0),
+                new FormField(
+                  builder: (FormFieldState state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                        icon: const Icon(Icons.person),
+                        labelText: 'Employees',
+                      ),
+                      isEmpty: employeeEmail == '',
+                      child: new DropdownButtonHideUnderline(
+                        child: new DropdownButton(
+                          value: employeeEmail,
+                          isDense: true,
+                          onChanged: (String newValue) {
+                            setState(() {
+                              employeeEmail = newValue;
+                              state.didChange(newValue);
+                            });
+                          },
+                          items: employeesEmails.map((String value) {
+                            return new DropdownMenuItem(
+                              value: value,
+                              child: new Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                // FlatButton(
+                //   onPressed: () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //         builder: (_) => ViewEmployeeList(),
+                //       ),
+                //     );
+                //   },
+                //   child: Text('Choose From Employee List'),
+                // ),
+                SizedBox(height: 20.0),
+                processing
+                    ? Center(child: CircularProgressIndicator())
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Material(
+                          elevation: 5.0,
+                          borderRadius: BorderRadius.circular(30.0),
+                          color: Theme.of(context).primaryColor,
+                          child: MaterialButton(
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                setState(() {
+                                  processing = true;
+                                });
+                                if (widget.taskModel != null) {
+                                  TaskModel taskModel = TaskModel(
                                     name: _nameController.text.trim(),
                                     description:
                                         _descriptionController.text.trim(),
-                                    taskDate: _taskDate,
-                                    aBid: widget.aBid,
-                                  ),
-                                );
+                                    id: widget.taskModel.id,
+                                  );
+                                  await NewTaskDB().updateTask(taskModel);
+                                } else {
+                                  await NewTaskDB().addNewTask(
+                                    TaskModel(
+                                      name: _nameController.text.trim(),
+                                      description:
+                                          _descriptionController.text.trim(),
+                                      taskDate: _taskDate,
+                                      aBid: widget.aBid,
+                                      employeeId:employeeEmail ,
+                                    ),
+                                  );
+                                  Navigator.pop(context);
+                                }
                                 Navigator.pop(context);
+                                setState(() {
+                                  processing = false;
+                                });
                               }
-                              Navigator.pop(context);
-                              setState(() {
-                                processing = false;
-                              });
-                            }
-                          },
-                          child: Text(isEditMode ? "Update" : "Save"),
+                            },
+                            child: Text(isEditMode ? "Update" : "Save"),
+                          ),
                         ),
                       ),
-                    ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-      //           ),
-      //           ViewEmployeeList(),
-      //         ],
-      //       ),
-      //     ),
-      //   ],
-      // ),
     );
   }
 
--- lib/screen/brandFiles/brandOpretion/tasks/viewTaskDetails.dart
+++ lib/screen/brandFiles/brandOpretion/tasks/viewTaskDetails.dart
@@ -24,6 +24,7 @@ class TaskDetailsPage extends StatelessWidget {
             SizedBox(height: 20.0),
             Text(task.description),
             SizedBox(height: 20.0),
+            Text(task.employeeId),
           ],
         ),
       ),
